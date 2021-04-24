9daf58e5f0bd905f7159536d7924b6af8564aea3
Maximilian Ehlert <Maximilian.Ehlert@Student.HTW-Berlin.de>
2021-04-24 14:26:02 +0200

    Ich habe einen ordner mit einer Eclopse datai hinzugefügt

---
+++ Exercise_1_Git/src/Git/Git.java
@@ -0,0 +1,20 @@
+
+
+public class Git {
+
+	public void main (String[] args) {
+		
+	}
+	// Should return true if you can reach exactly the goal(not more and not less) with the number of small and big bricks
+	//if you cannot reach exactly the goal with the bricks it should return false
+	
+	/**
+	 * @param smallBricks is 1 long
+	 * @param bigBricks is 5 long
+	 * @param goal 
+	 * @return
+	 */
+	public boolean makeBricks(int smallBricks, int bigBricks, int goal) {
+		return true;
+	}
+}
