4bd5c321da2faaf94b50ea4deb72d2741712b6a9
Maximilian Ehlert <Maximilian.Ehlert@Student.HTW-Berlin.de>
2021-04-24 15:25:24 +0200

    ich habe luckySum überarbeitet

--- Gitttytigitt/src/MainClass.java
+++ Gitttytigitt/src/MainClass.java
@@ -1,11 +1,18 @@
 
 public class MainClass {
-	public void main(String[] args) {
-		
+	public static void main(String[] args) {
+		System.out.println(luckySum(5,13,1));
 	}
-	public int luckySum(int a, int b, int c) {
-		throw new java.lang.UnsupportedOperationException("Not implemented yet.");
+	public static int luckySum(int a, int b, int c) {
+		int sum=0;
+		if (a!=13) {
+		  sum+=a;
+		  if(b!=13) {
+		    sum+=b;
+		    if(c!=13) sum+=c;
+		  }
+		}
+		return sum;
 		// Return sum of the values, but if one is 13, it does not count towards the value and neither to the values to the right of it.
 	}
-
 }
--- src/module-info.java
+++
@@ -1,5 +0,0 @@
-module Git01 {
-	public int sum13(int a, int b, int c ) {
-		throw new java.lang.UnsupportedOperationException ("Not implemented yet.");
-	}
-}
